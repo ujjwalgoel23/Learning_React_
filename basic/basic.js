43e0a3f66b071db6903318917de57bb049443ab7
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-06-07 18:20:40 +0530

    react

--- basic/basic.js
+++ basic/basic.js
@@ -1,4 +1,24 @@
-const element=React.createElement("h1",{},"hello world");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
+// const element=React.createElement("h1",{},"hello world");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
 
+// // creating  a element with id name and classname:-
+// const element1=React.createElement("h1",{className:"first",id:"fid"},"hello world");
+// const root1=ReactDOM.createRoot(document.getElementById("root"));
+// root1.render(element1);
+
+// // //also apply css on it
+// const element2=React.createElement("h1",{className:"second",id:"id",style:{fontSize:"50px"}},"hello world");
+// const root2=ReactDOM.createRoot(document.getElementById("root"));
+// root2.render(element2);
+
+// jaise hmne yha pr 3 baar render kiye hai toh actually screen pr 3rd wala he show hoga kyuki render krne se phle voo saara data remove ho jata hai automatically
+//agar ye tino data renderkrane hai ek sath he page me toh hm ek div bna kr or uss div ko root me render kra denge like this--->
+
+const element=React.createElement("h1",{},"hello world");
+const element1=React.createElement("h1",{className:"first",id:"fid"},"hello world");
+const element2=React.createElement("h1",{className:"second",id:"id",style:{fontSize:"50px"}},"hello world");
+const div1=React.createElement('div',{},[element,element1,element2]);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(div1);
