06f1afbfa01ea0494f0a5b61f6d40f091dec5b68
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-06-07 17:54:25 +0530

    react

--- basic.js
+++
@@ -1,3 +0,0 @@
-const element=React.createElement("h1",{},"hello world");
-const root=ReactDOM.createElement(document.getElementById("root"));
-root.render(element);
---
+++ basic/basic.js
@@ -0,0 +1,4 @@
+const element=React.createElement("h1",{},"hello world");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+
