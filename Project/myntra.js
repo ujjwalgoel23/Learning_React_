91ce56e0cf403d7f95ee90c7b48eafb7b40bd930
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-06-12 01:55:44 +0530

    react

---
+++ Project/myntra.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Card(props){
+    return (
+       <div style={{border:"2px solid black",padding:"2px"}}>
+        <img src="https://factori.com/assets/img/tshirt1.jpeg" height="200px" width="200px"/>
+          <div style={{textAlign:"center"}}>
+            <h2>{props.cloth}</h2>
+            <h1>{props.offer}</h1>
+            <h1>Shop Now</h1>
+          </div>
+       </div>
+    )   
+};
+
+
+const arr=[{cloth:"Skirt",offer:"60-80%off"},{cloth:"Jeans",offer:"70-80%off"},{cloth:"Kurta",offer:"40-80%off"},{cloth:"Pant",offer:"10-80%off"},{cloth:"Shoes",offer:"30-80%off"},{cloth:"Casual",offer:"30-50%off"},{cloth:"T-shirt",offer:"30-70%off"},{cloth:"Pant",offer:"20-60%off"}]
+function App(){
+    return (
+   <div style={{ display:"flex", gap:"10px" , flexWrap:"wrap"}}>
+    {/* <Card cloth="T-shirt" offer="20-80%off"/>
+    <Card cloth="shirt" offer="30-50%off"/>
+    <Card cloth="Pant" offer="30-80%off"/>
+    <Card cloth="Shoes" offer="70-80%off"/>
+    <Card cloth="Jeans" offer="10-30%off"/>
+    <Card cloth="Kurta" offer="10-50%off"/>
+    <Card cloth="Pajama" offer="20-60%off"/>
+    <Card cloth="Sneaker" offer="10-60%off"/>
+    <Card cloth="casual" offer="20-90%off"/>
+     */}
+     {
+      arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.offer}/>)
+     }
+   </div>
+    )
+};
+
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
