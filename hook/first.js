d2c1ffd231d30e91ed7be59ac41022a58c93c04c
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-06-15 13:51:43 +0530

    react

--- hook/first.js
+++ hook/first.js
@@ -3,22 +3,20 @@ import ReactDOM from "react-dom/client";
 
 function Counter(){
     // let count=0;
-    let [count,setCount]=useState(0); 
+    let [count,setCount] = useState(0); 
 
-function incrementnumber(){
+    function incrementnumber(){ 
      count=count+1;
      setCount(count);
    //  document.querySelector('h1').innerHTML=`count is ${count}`;//DOM ko manipulate kr rha hai
 //react kehta hai ki tumhe khud se DOM manipulate nhi krnaa
 // react optimise way me DOM ko manipulate krta hai isliye hm ye kaam react ko sopp dete hai
-
 }
 
 function decrementnumber(){
      count=count-1;
       setCount(count);
-   //  document.querySelector('h1').innerHTML=`count is ${count}`;//DOM ko manipulate kr rha hai
-     
+   //  document.querySelector('h1').innerHTML=`count is ${count}`;//DOM ko manipulate kr rha hai   
 }
 
    return (<div className="first">
