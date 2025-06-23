4c482f098338e29c68c5a04ba3012318b6302e38
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-06-23 21:51:55 +0530

    react

---
+++ Project_04/src/first.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./component/Header";
+import Body from "./component/Body";
+
+
+function Github_profile(){
+    return (
+        <>
+         <Header/>
+         <Body/>
+        </>
+    )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<Github_profile/>);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
