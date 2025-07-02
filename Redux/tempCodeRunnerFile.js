62222d1c440c4dbd2d23adaf9beeca47435b5c22
ujjwal goel <ujjwalgoel2711@gmail.com>
2025-07-02 10:19:26 +0530

    react

---
+++ Redux/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Counting from "./Counting"; 
+import { Provider } from "react-redux";
+import stores from "./Stores";
+import CustomCounter from "./CustomCounter";
+
+function App(){
+    return (
+        <Provider store={stores}>
+             <Counting/>
+             <br></br>
+             <br></br>
+             <br></br>
+             <CustomCounter/>
+        </Provider>
+    )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+
